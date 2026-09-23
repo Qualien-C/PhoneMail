4b3f5450014e911080fec0a3a997bf8174a31362
Qualien <qualien.cd@gmail.com>
2026-09-23 18:09:57 +0530

    initialize authentication middleware

--- server/src/server.js
+++ server/src/server.js
@@ -2,14 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
+dotenv.config();
+
 const connectDB = require("./config/db");
 
-dotenv.config();
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 connectDB();
 
